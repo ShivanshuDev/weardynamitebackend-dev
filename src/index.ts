d28f5217727d423c3bf924a3303be9c9eda7021d
Shivanshu <89439900+ShivanshuDev@users.noreply.github.com>
2026-04-13 19:13:18 +0530

    redis implemented

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,17 @@ const PORT = process.env.PORT || 3001;
 
 // ─── Middleware ────────────────────────────────────────────────────────────────
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5177', 'http://localhost:3000', "https://weardynamite.com", "http://www.weardynamite.com", "weardynamite.com"],
+  origin: [
+    'https://din4g8s0x2j11.cloudfront.net', 
+    'http://localhost:5173', 
+    'http://localhost:5174', 
+    'http://localhost:5175', 
+    'http://localhost:5177', 
+    'http://localhost:3000', 
+    "https://weardynamite.com",
+    "https://www.weardynamite.com", 
+    "http://www.weardynamite.com", 
+    "weardynamite.com"],
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
